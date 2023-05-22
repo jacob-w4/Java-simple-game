a746ce549ac6da1ba9dbfd9d89a169455083cae0
Jakub <jakubplewa00@gmail.com>
2023-05-22 20:54:03 +0200

    Changes by marek

--- src/main/java/com/simulation/Player.java
+++ src/main/java/com/simulation/Player.java
@@ -1,6 +1,7 @@
 package com.simulation;
 
 import javax.swing.*;
+import java.util.Objects;
 
 public class Player {
 
@@ -8,7 +9,27 @@ public class Player {
     private ImageIcon playerIcon;
     private int playerMoves;
 
-    public void movePlayer(){
+    public void movePlayer(int n, String dir){
+        if(Objects.equals(dir, "x")){
+            if (boardPosition[playerPosition[0]][playerPosition[1]+n] == 0){
+                oldPostion[0] = playerPosition[0];
+                oldPostion[1] = playerPosition[1];
+                System.out.println(oldPostion[0]);
+                System.out.println(oldPostion[1]);
+                playerPosition[1]+=n;
+            }
+
+        }
+        else if(Objects.equals(dir, "y")){
+            if (boardPosition[playerPosition[0]+n][playerPosition[1]] == 0){
+                oldPostion[0] = playerPosition[0];
+                oldPostion[1] = playerPosition[1];
+                System.out.println(oldPostion[0]);
+                System.out.println(oldPostion[1]);
+                playerPosition[0]+=n;
+            }
+
+        }
 
     }
 
