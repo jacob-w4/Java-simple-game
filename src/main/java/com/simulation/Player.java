c5757905f5a73155543c90584f75e12104747264
Jakub <jakubplewa00@gmail.com>
2023-05-22 21:00:49 +0200

    0.0.1

--- src/main/java/com/simulation/Player.java
+++ src/main/java/com/simulation/Player.java
@@ -17,6 +17,7 @@ public class Player extends Entity {
     public void movePlayer(int n, String dir) {
         if (Objects.equals(dir, "x")) {
             if (boardPosition[playerPosition[0]][playerPosition[1] + n] == 0) {
+
                 oldPostion[0] = playerPosition[0];
                 oldPostion[1] = playerPosition[1];
                 System.out.println(oldPostion[0]);
